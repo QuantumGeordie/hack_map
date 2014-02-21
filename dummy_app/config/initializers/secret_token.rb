bdc89cb71248008381b1be54c0980af4745a3259
Michael Jewell <michael.jewell@appfolio.com>
2014-02-20 18:51:48 -0800

    server and map stuff, again, yo

---
+++ dummy_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyApp::Application.config.secret_token = 'f2bc003969c17967380d77ca8951717d1e196b7c7fd5cc6dc0debdf726efe01f66113e43cbb158bfdcfda53e7f2e65152550c589f7f9f23d627534392373d909'
